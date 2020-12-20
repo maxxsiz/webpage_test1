f8aac57ada37a5cbacbe07ab343de47e49a9c194
maxxsiz <werfeteiii@gmail.com>
2020-12-20 02:01:22 +0100

    contact block part 1 (create main block and button open and close down)

--- blog/static/blog/js/script_1.js
+++ blog/static/blog/js/script_1.js
@@ -130,3 +130,19 @@
         }
 
     }
+
+    var is_email_contact_open = "0";
+    function contact_email(){
+        if (is_email_contact_open == "0"){
+            document.getElementById("email-contact-big-block").style.display = 'block';
+            document.getElementById("email-contact-big-block").style.opacity = '1';
+            is_email_contact_open = "1";
+            return console.log('open contact block');
+        }
+        if (is_email_contact_open == "1"){
+            document.getElementById("email-contact-big-block").style.display = 'none';
+            document.getElementById("email-contact-big-block").style.opacity = '0';
+            is_email_contact_open = "0";
+            return console.log('close contact block');
+        }
+    }
